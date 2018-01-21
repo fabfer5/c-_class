589913e108cfc9fb4d313de30861af9ee6f42028
fabian <fernandezfabian213546879@gmail.com>
2018-01-21 14:54:20 -0600

    it is finished

--- Problem1/lab1_favian_travelTime.cpp/main.cpp
+++ Problem1/lab1_favian_travelTime.cpp/main.cpp
@@ -1,25 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// CMPSC125  
+// Project:  Lab 1
+// Author: Favian Fernande
+// File Name: lab1_favian_convertCentimeter.cpp
+// Date:  Jan 21, 2018
+/*Description: Write a program that takes length as input in feet and inches. 
+The program should then convert the lengths in centimeters and display it on screen. 
+Use the following named constants.*/
 
-/* 
- * File:   main.cpp
- * Author: faian
- *
- * Created on January 20, 2018, 5:31 PM
- */
-
-#include <cstdlib>
-
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
 using namespace std;
+const double CENTIMETERS_PER_INCH = 2.54;	//Named constants
+const int INCHES_PER_FOOT = 12;			//Named constants
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
-    return 0;
+int main ()
+{
+	int feet, inches;
+	int totalInches;
+	double centimeter;
+        
+	cout << "Enter two integers, one for feet and one for inches: "; 
+	cin >> feet >> inches;
+	cout << endl;
+        
+	cout << "The numbers you entered are " << feet << " for feet and " << inches << " for inches. " << endl; 
+	
+        totalInches = INCHES_PER_FOOT * feet + inches;
+	cout << "The total number of inches = " << totalInches <<" inches"<< endl;
+	centimeter = CENTIMETERS_PER_INCH * totalInches;
+	cout << "The number of centimeters = " << centimeter <<" inches"<< endl;
+return 0;
 }
-
